3bc6afb2286f9dfb41a6baadac3adb26408c5830
JeongEun <lisasje25@gmail.com>
2019-07-30 18:26:53 +0900

    5

--- board/settings.py
+++ board/settings.py
@@ -118,12 +118,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 
 STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'board/static'),
 )
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
